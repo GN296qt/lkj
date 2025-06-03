6f4776dad6794d607c0573979a03f104e55eaed1
Gorli Gomati Ramya <g.gomatiramyaa@gmail.com>
2025-06-03 14:59:28 +0530

    while loop

---
+++ loops/whileloop/sample.java
@@ -0,0 +1,12 @@
+package loops.whileloop;
+
+public class sample {
+    public static void main(String[] args) {
+         int i = 1 ;//intialization
+         while(i<=10){// condition 
+            System.out.print(i+" ");// body of loop
+            i++;// updation
+         }
+    }
+}
+//1 2 3 4 5 6 7 8 9 10 
