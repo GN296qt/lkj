8e04e21d7171a00de2ef60cf90e64b4fc5a2868a
Gorli Gomati Ramya <g.gomatiramyaa@gmail.com>
2025-06-03 15:07:03 +0530

    dowhile loop

---
+++ loops/dowhileloop/sample.java
@@ -0,0 +1,12 @@
+package loops.dowhileloop;
+
+public class sample {
+    public static void main(String[] args) {
+        int i = 1;//intialization
+        do{
+              System.out.print(i+" ");//body of loop
+              i++;// updation
+        }while(i<=10);// condition
+    }
+}
+// 1 2 3 4 5 6 7 8 9 10 
